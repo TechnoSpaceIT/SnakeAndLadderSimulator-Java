e589f18a25bfc30031f65ddcb87b4f006dbfb126
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-15 21:07:43 +0530

    [MODIFIED]:[Tahir] Rolling Die for Next Position

--- src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -1,5 +1,7 @@
 package com.infogalaxy.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
@@ -7,9 +9,19 @@ public class SnakeAndLadder {
 		 * Variable for defining player position in game
 		 */
 		int position = 0;
+		/**
+		 * Random class object to generate Die Number between 1-6
+		 */
+		Random random = new Random();
 		
-		System.out.println("Player 1 Position is = "+position);
+		System.out.println("Player 1 Start Position is = "+position);
 
+		int dieNo = random.nextInt(6)+1;
+		System.out.println("Die Number="+dieNo);
+		
+		position = position + dieNo;
+		
+		System.out.println("New Position After Rolling Die = "+position);
 	}
 
 }
