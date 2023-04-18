2979d559cd76ced929309f98612e96e3a198be93
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-18 22:48:09 +0530

    [MODIFIED]:[Tahir] Playing with 2 Players

--- src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -9,7 +9,9 @@ public class SnakeAndLadder {
 		 * Variable for defining player position in game
 		 */
 		int position = 0;
+		int position2 = 0;
 		int count_dieRoll = 0;
+		int chance = 1;
 		final int NO_PLAY = 1;
 		final int LADDER = 2;
 		final int SNAKE = 3;
@@ -19,38 +21,79 @@ public class SnakeAndLadder {
 		Random random = new Random();
 		
 		System.out.println("Player 1 Start Position is = "+position);
+		System.out.println("Player 2 Start Position is = "+position2);
 
 		while(position != 100) {
 		
-			int dieNo = random.nextInt(6)+1;
-			System.out.println("Die Number="+dieNo);
-			
-			count_dieRoll++;
-			
-			int option = random.nextInt(3)+1;
-			System.out.println("Option Number="+option);
-			switch(option) {
-			case NO_PLAY:
-					System.out.println("Option = NO PLAY");
-					position = position;
-				break;
-			case LADDER:
-					System.out.println("Option = LADDER");
-					position = position + dieNo;
-					if(position > 100) {
+			if(chance % 2 == 0) {
+				System.out.println("Player 1 Chance");
+				int dieNo = random.nextInt(6)+1;
+				System.out.println("Die Number="+dieNo);
+				
+				count_dieRoll++;
+				
+				int option = random.nextInt(3)+1;
+				System.out.println("Option Number="+option);
+				switch(option) {
+				case NO_PLAY:
+						System.out.println("Option = NO PLAY");
+						position = position;
+					break;
+				case LADDER:
+						System.out.println("Option = LADDER");
+						position = position + dieNo;
+						if(position > 100) {
+							position = position - dieNo;
+						}
+						chance--;
+					break;
+				case SNAKE:
+						System.out.println("Option = SNAKE");
 						position = position - dieNo;
-					}
-				break;
-			case SNAKE:
-					System.out.println("Option = SNAKE");
-					position = position - dieNo;
-					if(position < 0) {
-						position = 0;
-					}
-				break;
+						if(position < 0) {
+							position = 0;
+						}
+					break;
+				}
+				System.out.println("New Position for Player 1= "+position);
+				System.out.println("##########################");
 			}
-			System.out.println("New Position = "+position);
-			System.out.println("##########################");
+			else {
+				System.out.println("Player 2 Chance");
+
+				int dieNo = random.nextInt(6)+1;
+				System.out.println("Die Number="+dieNo);
+				
+				count_dieRoll++;
+				
+				int option = random.nextInt(3)+1;
+				System.out.println("Option Number="+option);
+				switch(option) {
+				case NO_PLAY:
+						System.out.println("Option = NO PLAY");
+						position2 = position2;
+					break;
+				case LADDER:
+						System.out.println("Option = LADDER");
+						position2 = position2 + dieNo;
+						if(position2 > 100) {
+							position2 = position2 - dieNo;
+						}
+						chance--;
+					break;
+				case SNAKE:
+						System.out.println("Option = SNAKE");
+						position2 = position2 - dieNo;
+						if(position2 < 0) {
+							position2 = 0;
+						}
+					break;
+				}
+				System.out.println("New Position for Player2= "+position2);
+				System.out.println("##########################");
+			}
+			chance++;
+			//System.out.println("Chance="+chance);
 		}
 		System.out.println("Total Count of Die Roll to Win the Game = "+count_dieRoll);
 	}
