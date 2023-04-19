0d6b662835f91a39fa42986c697f62da54766314
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-19 21:41:23 +0530

    [MODIFIED]:[Tahir] Playing with 2 Players using Object Oriented

---
+++ src/com/infogalaxy/snakeandladder/Constants.java
@@ -0,0 +1,9 @@
+package com.infogalaxy.snakeandladder;
+
+public class Constants {
+
+	final static int NO_PLAY = 1;
+	final static int LADDER = 2;
+	final static int SNAKE = 3;
+	
+}
--- src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
+++ src/com/infogalaxy/snakeandladder/SnakeAndLadder.java
@@ -3,99 +3,54 @@ package com.infogalaxy.snakeandladder;
 import java.util.Random;
 
 public class SnakeAndLadder {
-
-	public static void main(String[] args) {
-		/**
-		 * Variable for defining player position in game
-		 */
-		int position = 0;
-		int position2 = 0;
-		int count_dieRoll = 0;
-		int chance = 1;
-		final int NO_PLAY = 1;
-		final int LADDER = 2;
-		final int SNAKE = 3;
-		/**
-		 * Random class object to generate Die Number between 1-6
-		 */
-		Random random = new Random();
-		
-		System.out.println("Player 1 Start Position is = "+position);
-		System.out.println("Player 2 Start Position is = "+position2);
-
-		while(position != 100) {
-		
-			if(chance % 2 == 0) {
-				System.out.println("Player 1 Chance");
-				int dieNo = random.nextInt(6)+1;
-				System.out.println("Die Number="+dieNo);
-				
-				count_dieRoll++;
-				
-				int option = random.nextInt(3)+1;
-				System.out.println("Option Number="+option);
-				switch(option) {
-				case NO_PLAY:
-						System.out.println("Option = NO PLAY");
-						position = position;
-					break;
-				case LADDER:
-						System.out.println("Option = LADDER");
-						position = position + dieNo;
-						if(position > 100) {
-							position = position - dieNo;
-						}
-						chance--;
-					break;
-				case SNAKE:
-						System.out.println("Option = SNAKE");
-						position = position - dieNo;
-						if(position < 0) {
-							position = 0;
-						}
-					break;
-				}
-				System.out.println("New Position for Player 1= "+position);
-				System.out.println("##########################");
-			}
-			else {
-				System.out.println("Player 2 Chance");
-
-				int dieNo = random.nextInt(6)+1;
-				System.out.println("Die Number="+dieNo);
-				
-				count_dieRoll++;
-				
-				int option = random.nextInt(3)+1;
-				System.out.println("Option Number="+option);
-				switch(option) {
-				case NO_PLAY:
-						System.out.println("Option = NO PLAY");
-						position2 = position2;
-					break;
-				case LADDER:
-						System.out.println("Option = LADDER");
-						position2 = position2 + dieNo;
-						if(position2 > 100) {
-							position2 = position2 - dieNo;
-						}
-						chance--;
-					break;
-				case SNAKE:
-						System.out.println("Option = SNAKE");
-						position2 = position2 - dieNo;
-						if(position2 < 0) {
-							position2 = 0;
-						}
-					break;
-				}
-				System.out.println("New Position for Player2= "+position2);
-				System.out.println("##########################");
-			}
-			chance++;
-			//System.out.println("Chance="+chance);
-		}
-		System.out.println("Total Count of Die Roll to Win the Game = "+count_dieRoll);
+	
+	int position = 0;
+	
+	Random random = new Random();
+	
+	public void showPosition(int no) {
+		System.out.println("Player "+no+" Position is = "+position);		
 	}
 
+	public void playGame(int no) {
+		
+		int dieNo = random.nextInt(6)+1;
+		System.out.println("Die Number="+dieNo);
+		
+		int option = random.nextInt(3)+1;
+		System.out.println("Option Number="+option);
+		switch(option) {
+		case Constants.NO_PLAY:
+				System.out.println("Option = NO PLAY");
+				position = position;
+			break;
+		case Constants.LADDER:
+				System.out.println("Option = LADDER");
+				position = position + dieNo;
+				if(position > 100) {
+					position = position - dieNo;
+				}
+			break;
+		case Constants.SNAKE:
+				System.out.println("Option = SNAKE");
+				position = position - dieNo;
+				if(position < 0) {
+					position = 0;
+				}
+			break;
+		}
+		showPosition(no);
+		System.out.println("##########################");
+	}
+	
+	public static void main(String[] args) {
+		SnakeAndLadder player1 = new SnakeAndLadder();
+		SnakeAndLadder player2 = new SnakeAndLadder();
+		player1.showPosition(1);
+		player2.showPosition(2);
+		while(player1.position != 100 || player2.position != 100) {
+			player1.playGame(1);
+			player2.playGame(2);
+		}
+	}
 }
